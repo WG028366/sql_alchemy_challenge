157647236d5f706af6e09b250e9862136eb39f15
William <william.garrettiv@outlook.com>
2020-04-10 11:03:42 -0500

    resubmit try

---
+++ app.py
@@ -0,0 +1,89 @@
+
+import numpy as np
+
+
+
+import sqlalchemy
+
+from sqlalchemy.ext.automap import automap_base
+
+from sqlalchemy.orm import Session
+
+from sqlalchemy import create_engine, func
+
+
+
+from flask import Flask, jsonify
+
+
+
+#################################################
+
+# Database Setup
+
+#################################################
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite") 
+
+
+
+# reflect an existing database into a new model
+
+Base = automap_base()
+
+# reflect the tables
+
+Base.prepare(engine, reflect=True)
+
+
+
+# Save reference to the table
+
+measurement = Base.classes.measurement
+
+station = Base.classes.station
+
+
+
+#################################################
+
+# Flask Setup
+
+#################################################
+
+app = Flask(__name__)
+
+
+
+
+
+#################################################
+
+# Flask Routes
+
+
+
+@app.route("/")
+
+def index():
+
+    return (
+
+        f"Welcome to the Hawaii Weather API!<br/>"
+
+        f"Available Routes:<br/>"
+
+        f"/api/v1.0/precipitation<br/>"
+
+        f"/api/v1.0/stations<br/>"
+
+        f"/api/v1.0/tobs<br/>"
+
+        f"/api/v1.0/start_date(YYYY-MM-DD)/end_date(YYYY-MM-DD)"
+
+    )
+
+    
+if __name__ == "__main__":
+
+    app.run(debug=True)
